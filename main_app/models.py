d2ab35b4ea482ae6c58aad920e14857c2e95f059
kmcclendon <kmcclendon56@gmail.com>
2022-09-06 12:44:49 -0500

    Added quiz and questions

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db import reverse
 from django.contrib.auth.models import User
+#Need to import Profile
 
 #================================================================================================#
 
@@ -10,9 +11,17 @@ from django.contrib.auth.models import User
 
 # class Card(models.Model):
 
+
 # class Quiz(models.Model):
+class Quiz(models.Model):
+    title = models.CharField(max_length=100)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 # class Questions(models.Model):
+class Question(models.Model):
+    question = models.CharField(max_length=250)
+    answer = models.TextField(max_length=250)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
 
 # class Photo(models.Model):
 #     url = models.CharField(max_length=200)
@@ -23,11 +32,6 @@ from django.contrib.auth.models import User
 #         return f"photo for card_id: {self.flashcard_id} @{self.url}"
 #         return f"photo for question_id: {self.flashcard_id} @{self.url}"
 
-class Quiz(models.Model):
-  title = models.CharField(max_length=100)
-  question = models.CharField(max_length=250)
-  answer = models.TextField(max_length=250)
-  pofile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 
 
